7ceecebd7df13434ef394f757697ed8bd5604b25
Sahdab Ansari <sahdabansari972@gmail.com>
2026-03-04 10:30:42 +0530

    chore(next): update Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,19 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+
 };
 
 export default nextConfig;
